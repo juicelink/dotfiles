fd32463af09a968b96ca6161f6671fb203ebe137
juicelink <chezju@gmail.com>
2016-10-30 19:19:42 +0100

    add vim 8

--- apt.sh
+++ apt.sh
@@ -4,6 +4,51 @@ packages=(
   gawk
   git
   zsh
+  libncurses5-dev
+  libgnome2-dev
+  libgnomeui-dev
+  libgtk2.0-dev
+  libatk1.0-dev 
+  libbonoboui2-dev
+  libcairo2-dev
+  libx11-dev
+  libxpm-dev
+  libxt-dev
+  python-dev
+  python3-dev
+  ruby-dev
+  lua5.1
+  lua5.1-dev
+  libperl-dev
+  checkinstall
 )
 
 sudo apt-get install ${packages[@]}
+
+sudo apt-get remove vim vim-runtime gvim vim-tiny vim-common vim-gui-common vim-nox
+
+cd ~
+
+git clone https://github.com/vim/vim.git
+
+cd vim
+
+./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp \
+            --enable-pythoninterp \
+            --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+            --enable-python3interp \
+            --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
+            --enable-perlinterp \
+            --enable-luainterp \
+            --enable-gui=gtk2 --enable-cscope --prefix=/usr
+
+make VIMRUNTIMEDIR=/usr/share/vim/vim80
+
+sudo checkinstall
+
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
+sudo update-alternatives --set editor /usr/bin/vim
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
+sudo update-alternatives --set vi /usr/bin/vim
